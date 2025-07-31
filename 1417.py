56c1acd575d09c1ba040aac08eaee87cb2aff27f
yjeong <yonghojeong57@gmail.com>
2025-07-31 13:06:04 +0900

    1418번 문제 해결

--- 1417.py
+++ 1417.py
@@ -1,7 +1,20 @@
 def main():
     N =int(input())
+    dasom = int(input())
     numbers = []
-    for i in range(N):
-        numbers[i] = int(input())
-    print(' '.join(map(int, numbers)))
+    answer = 0
+    if(N ==1):
+        print(answer)
+    else:
+        for _ in range(N-1):
+            numbers.append(int(input()))
+        while(True):
+            numbers.sort(reverse=True)
+            if numbers[0] < dasom:
+                break
+            numbers[0]-=1
+            dasom+=1
+            answer+=1
+        print(answer)
+
 main()

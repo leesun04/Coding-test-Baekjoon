afc82b78dab2344efa4b98479dfb386266c8fbec
leesun04 <lth9913@naver.com>
2025-08-21 17:17:15 +0900

    1264번 성공

--- bronze/1264.py
+++ bronze/1264.py
@@ -1,9 +1,13 @@
 def main():
     nums = ["a", "e", "i", "o", "u"]
     while(True):
-        quizs = input()
-        if nums in quizs:
-            print(1)
-        elif quizs == "#":
+        answer = 0
+        quizs = input().lower()
+        quizs_list = list(quizs)
+        for i in range(len(quizs_list)):
+            if quizs_list[i] in nums:
+                answer+=1         
+        if quizs == "#":
             break
+        print(answer)
 main()

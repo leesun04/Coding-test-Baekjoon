6428bb1b883591bafa49c8996f1beacba5408faf
leesun04 <lth9913@naver.com>
2025-09-02 10:03:30 +0900

    1267번 성공 & 사칙연산 즉 수학 조심!

--- bronze/1267.py
+++ bronze/1267.py
@@ -3,5 +3,14 @@ def main():
     nums = list(map(int, input().split()))
     y_nums = []
     m_nums = []
-    
+    for i in range(N):
+        y_nums.append((nums[i]//30+1)*10)
+        m_nums.append((nums[i]//60+1)*15)
+
+    if sum(y_nums) < sum(m_nums):
+        print(f"Y {sum(y_nums)}")
+    elif sum(y_nums) > sum(m_nums):
+        print(f"M {sum(m_nums)}")
+    else:
+        print(f"Y M {sum(y_nums)}")
 main()
